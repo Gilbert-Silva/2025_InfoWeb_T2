c58b06de2ab9f9a88dbc64b333a62a3e5e807490
Gilbert-Silva <gilbertif1995@outlook.com>
2025-07-03 13:08:58 +0000

    Lista 4

--- Aula_11/lista04ex02.py
+++ Aula_11/lista04ex02.py
@@ -4,6 +4,8 @@ class Contato:
         self.__nome = n
         self.__email = e
         self.__fone = f
+    def get_id(self):
+        return self.__id    
     def get_nome(self):
         return self.__nome    
     def __str__(self):
@@ -33,17 +35,42 @@ class ContatoUI:
         fone = input("Informe o fone: ")
         c = Contato(id, nome, email, fone)
         cls.__contatos.append(c)
+
     @classmethod    
     def listar(cls):
         #print(cls.__contatos)
+        if len(cls.__contatos) == 0: print("Nenhum contato cadastrado")
         for c in cls.__contatos:
             print(c)
+
+    @classmethod    
+    def listar_id(cls, id):
+        for c in cls.__contatos:
+            if c.get_id() == id: return c
+        return None    
+
     @classmethod    
     def atualizar(cls):
-        pass
+        cls.listar()
+        id = int(input("Informe o id do contato a ser atualizado: "))
+        c = cls.listar_id(id)
+        if c == None: print("Contato não encontrado")
+        else:
+            cls.__contatos.remove(c) 
+            nome = input("Informe o novo nome: ")
+            email = input("Informe o novo e-mail: ")
+            fone = input("Informe o novo fone: ")
+            c = Contato(id, nome, email, fone)
+            cls.__contatos.append(c)
+
     @classmethod    
     def excluir(cls):
-        pass
+        cls.listar()
+        id = int(input("Informe o id do contato a ser excluído: "))
+        c = cls.listar_id(id)
+        if c == None: print("Contato não encontrado")
+        else: cls.__contatos.remove(c)
+
     @classmethod    
     def pesquisar(cls):
         nome = input("Informe o nome: ")
